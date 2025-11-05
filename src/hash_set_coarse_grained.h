a9737db8585bcc1a2fb884f82db821550bc6eeb7
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-11-05 20:17:04 +0000

    finished sequential, started coarse grained

--- src/hash_set_coarse_grained.h
+++ src/hash_set_coarse_grained.h
@@ -1,41 +1,93 @@
 #ifndef HASH_SET_COARSE_GRAINED_H
 #define HASH_SET_COARSE_GRAINED_H
 
-#include <cassert>
-#include <vector>
-#include <functional>
 #include <algorithm>
+#include <cassert>
+#include <functional>
+#include <vector>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetCoarseGrained : public HashSetBase<T> {
  public:
-  explicit HashSetCoarseGrained(size_t /*initial_capacity*/) {}
-
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  explicit HashSetSequential(const size_t capacity)
+      : table_(capacity), set_size_(0) {
+    assert(capacity > 0);
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Add(T elem) final {
+    auto& bucket = Bucket_(elem);
+
+    // 3) return false on duplicate (loops over the elements in that bucket)
+    if (std::find(bucket.begin(), bucket.end(), elem) != bucket.end()) {
+      return false;
+    }
+
+    // 4) insert and update size if not present
+    bucket.push_back(std::move(elem));
+    set_size_++;
+
+    // 5) apply resizing policy if needed
+    if (Policy_()) {
+      Resize_();
+    }
+    return true;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Remove(T elem) final {
+    // compute bucket index & find bucket
+    auto& bucket = Bucket_(elem);
+
+    // find element position (returning early if doesn't exist)
+    auto i = std::find(bucket.begin(), bucket.end(), elem);
+    if (i == bucket.end()) return false;
+
+    // remove element & decrement size
+    bucket.erase(i);
+    set_size_--;
+    return true;
   }
 
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+  [[nodiscard]] bool Contains(T elem) final {
+    auto& bucket = Bucket_(elem);
+
+    // return if found or not
+    auto found = std::find(bucket.begin(), bucket.end(), elem) != bucket.end();
+    return found;
   }
+
+  [[nodiscard]] size_t Size() const final { return set_size_; }
+
  private:
   std::vector<std::vector<T>> table_;
-  size_t set_size_; //tracks the number of elements in the table
+  size_t set_size_;  // tracks the number of elements in the table
   std::hash<T> hasher_;
+
+  /**
+   * Returns the bucket associated with the element.
+   */
+  std::vector<T>& Bucket_(T elem) {
+    return table_[hasher_(elem) % table_.size()];
+  }
+
+  bool Policy_() { return set_size_ / table_.size() > 4; }
+
+  void Resize_() {
+    // 1) create a new empty table with double the number of buckets
+    std::vector<std::vector<T>> new_table(table_.size() * 2);
+
+    // 2) move elements from the old table to the new one
+    for (auto& bucket : table_) {
+      for (auto& elem : bucket) {
+        size_t i = hasher_(elem) % new_table.size();
+        new_table[i].push_back(std::move(elem));
+      }
+    }
+
+    // 3) replace old table with new one
+    table_ = std::move(new_table);
+  }
 };
 
 #endif  // HASH_SET_COARSE_GRAINED_H
--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -1,30 +1,27 @@
 #ifndef HASH_SET_SEQUENTIAL_H
 #define HASH_SET_SEQUENTIAL_H
 
-#include <cassert>
-#include <vector>
-#include <functional>
 #include <algorithm>
+#include <cassert>
+#include <functional>
+#include <vector>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetSequential : public HashSetBase<T> {
  public:
-  explicit HashSetSequential(const size_t capacity) : table_(capacity), set_size_(0) {
+  explicit HashSetSequential(const size_t capacity)
+      : table_(capacity), set_size_(0) {
     assert(capacity > 0);
   }
 
   bool Add(T elem) final {
-    // 1) compute bucket index
-    const size_t i = hasher_(elem) % table_.size();
-
-    // 2) refer to that bucket
-    auto& bucket = table_[i];
+    auto& bucket = Bucket_(elem);
 
     // 3) return false on duplicate (loops over the elements in that bucket)
-    for (const auto& x : bucket) {
-      if (x == elem) return false;
+    if (std::find(bucket.begin(), bucket.end(), elem) != bucket.end()) {
+      return false;
     }
 
     // 4) insert and update size if not present
@@ -40,15 +37,14 @@ class HashSetSequential : public HashSetBase<T> {
 
   bool Remove(T elem) final {
     // compute bucket index & find bucket
-    auto i = hasher_(elem) % table_.size();
-    auto& bucket = table_[i];
+    auto& bucket = Bucket_(elem);
 
     // find element position (returning early if doesn't exist)
-    auto pos = std::find(bucket.begin(), bucket.end(), elem);
-    if (pos == bucket.end()) return false;
+    auto i = std::find(bucket.begin(), bucket.end(), elem);
+    if (i == bucket.end()) return false;
 
     // remove element & decrement size
-    bucket.erase(pos);
+    bucket.erase(i);
     set_size_--;
     return true;
   }
@@ -61,41 +57,37 @@ class HashSetSequential : public HashSetBase<T> {
     return found;
   }
 
-  [[nodiscard]] size_t Size() const final {
-    return set_size_;
+  [[nodiscard]] size_t Size() const final { return set_size_; }
+
+ private:
+  std::vector<std::vector<T>> table_;
+  size_t set_size_;  // tracks the number of elements in the table
+  std::hash<T> hasher_;
+
+  /**
+   * Returns the bucket associated with the element.
+   */
+  std::vector<T>& Bucket_(T elem) {
+    return table_[hasher_(elem) % table_.size()];
   }
 
-  private:
-    std::vector<std::vector<T>> table_;
-    size_t set_size_; //tracks the number of elements in the table
-    std::hash<T> hasher_;
+  bool Policy_() { return set_size_ / table_.size() > 4; }
 
-    bool Policy_() {
-      return set_size_ / table_.size() > 4;
-    }
+  void Resize_() {
+    // 1) create a new empty table with double the number of buckets
+    std::vector<std::vector<T>> new_table(table_.size() * 2);
 
-    void Resize_() {
-      // 1) create a new empty table with double the number of buckets
-      std::vector<std::vector<T>> new_table(table_.size() * 2);
-
-      // 2) move elements from the old table to the new one
-      for (auto& bucket : table_) {
-        for (auto& elem : bucket) {
-          size_t i = std::hash<T>()(elem) % new_table.size();
-          new_table[i].push_back(std::move(elem));
-        }
+    // 2) move elements from the old table to the new one
+    for (auto& bucket : table_) {
+      for (auto& elem : bucket) {
+        size_t i = hasher_(elem) % new_table.size();
+        new_table[i].push_back(std::move(elem));
       }
-
-      // 3) replace old table with new one
-      table_ = std::move(new_table);
     }
 
-    /**
-     * Returns the bucket associated with the element.
-     */
-    std::vector<T>& Bucket_(T elem) {
-      return table_[hasher_(elem) % table_.size()];
-    }
+    // 3) replace old table with new one
+    table_ = std::move(new_table);
+  }
 };
 
 #endif  // HASH_SET_SEQUENTIAL_H
