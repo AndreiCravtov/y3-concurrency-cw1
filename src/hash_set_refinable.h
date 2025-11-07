12aa7cd70e43928495e26d4bbb477185dbf82972
Mikhail Bazarsadaev <mbazarsadaev@yandex.ru>
2025-11-07 03:11:29 +0000

    works 50% of the time

--- src/hash_set_refinable.h
+++ src/hash_set_refinable.h
@@ -2,6 +2,8 @@
 #define HASH_SET_REFINABLE_H
 
 #include <cassert>
+#include <iostream>
+#include <memory>
 #include <mutex>
 #include <thread>
 #include <vector>
@@ -17,15 +19,19 @@ class HashSetRefinable : public HashSetBase<T> {
         table_size_(capacity),
         size_(0),
         mutexes_(capacity),
-        owner_(nullptr) {
+        is_resizing_(false) {
     assert(capacity > 0);
+    mutexes_.resize(capacity);
+    for (auto& m : mutexes_) {
+      m = std::make_shared<std::mutex>();
+    }
   }
 
   bool Add(T elem) final {
     // Acquire Lock
     Acquire_(elem);
 
-    std::vector<T> bucket = Bucket_(elem);
+    std::vector<T>& bucket = Bucket_(elem);
 
     if (std::find(bucket.begin(), bucket.end(), elem) != bucket.end()) {
       Release_(elem);
@@ -43,7 +49,7 @@ class HashSetRefinable : public HashSetBase<T> {
   bool Remove(T elem) final {
     Acquire_(elem);
 
-    std::vector<T> bucket = Bucket_(elem);
+    std::vector<T>& bucket = Bucket_(elem);
 
     // find element position (returning early if doesn't exist)
     auto i = std::find(bucket.begin(), bucket.end(), elem);
@@ -62,7 +68,7 @@ class HashSetRefinable : public HashSetBase<T> {
   [[nodiscard]] bool Contains(T elem) final {
     Acquire_(elem);
 
-    std::vector<T> bucket = Bucket_(elem);
+    std::vector<T>& bucket = Bucket_(elem);
 
     if (std::find(bucket.begin(), bucket.end(), elem) == bucket.end()) {
       Release_(elem);
@@ -79,14 +85,11 @@ class HashSetRefinable : public HashSetBase<T> {
   std::vector<std::vector<T>> table_;
   std::atomic<size_t> table_size_;
   std::atomic<size_t> size_;
-  std::vector<std::mutex> mutexes_;
+  std::vector<std::shared_ptr<std::mutex>> mutexes_;
+
   std::hash<T> hasher_;
 
-  /**
-   * Contains Ptr to std::thread::id of the thread that owns lock
-   * False - no one uses, True - someone is using Hashset
-   */
-  AtomicMarkablePtr<std::thread::id> owner_;
+  std::atomic<bool> is_resizing_;
 
   /**
    * Returns the bucket associated with the element
@@ -98,8 +101,9 @@ class HashSetRefinable : public HashSetBase<T> {
   /**
    * Returns the mutex associated with the element's bucket
    */
-  std::mutex& Mutex_(T elem) {
-    return mutexes_[hasher_(elem) % table_size_.load()];
+  std::shared_ptr<std::mutex> Mutex_(T elem) {
+    size_t idx = hasher_(elem) % table_size_.load();
+    return mutexes_[idx];  // returns shared_ptr copy
   }
 
   /**
@@ -109,20 +113,14 @@ class HashSetRefinable : public HashSetBase<T> {
    * 3) Acquires lock and checks again (1)
    */
   void Acquire_(T elem) {
-    thread_local std::thread::id me = std::this_thread::get_id();
-    std::thread::id* who;
-    bool mark = true;
     while (true) {
-      do {
-        who = owner_.Get(mark);
-      } while (mark && *who != me);
+      while (is_resizing_.load()) {
+      }
 
-      std::mutex& mutex = Mutex_(elem);
+      std::mutex& mutex = *Mutex_(elem);
       mutex.lock();
 
-      who = owner_.Get(mark);
-
-      if ((!mark || *who != me) /* && old_locks == locks_ */) {
+      if (!is_resizing_.load() /* && old_locks == locks_ */) {
         return;
       }
       mutex.unlock();
@@ -133,14 +131,17 @@ class HashSetRefinable : public HashSetBase<T> {
   /**
    * Releases corresponding to the item lock
    */
-  void Release_(T elem) { Mutex_(elem).unlock(); }
+  void Release_(T elem) {
+    auto mtx = Mutex_(elem);
+    mtx->unlock();
+  }
 
   /**
    * Waits for all locks to be unlocked
    */
   void Quiesce_() {
-    for (auto& mutex : mutexes_) {
-      std::scoped_lock<std::mutex> lock(mutex);
+    for (auto& mtx_ptr : mutexes_) {
+      std::scoped_lock<std::mutex> lock(*mtx_ptr);
     }
   }
 
@@ -154,10 +155,11 @@ class HashSetRefinable : public HashSetBase<T> {
   void Resize_() {
     size_t old_table_size = table_size_.load();
     size_t new_table_size = old_table_size * 2;
-    // bool mark = false;
-    thread_local std::thread::id me = std::this_thread::get_id();
 
-    if (owner_.CompareAndSet(nullptr, &me, false, true)) {
+    bool expected = false;
+    bool desired = true;
+
+    if (is_resizing_.compare_exchange_strong(expected, desired)) {
       if (table_size_.load() != old_table_size) {
         // someone already resized, just return
         return;
@@ -168,7 +170,12 @@ class HashSetRefinable : public HashSetBase<T> {
 
       std::vector<std::vector<T>> old_table = table_;
       table_ = std::vector<std::vector<T>>(new_table_size);
-      mutexes_ = std::vector<std::mutex>(new_table_size);
+
+      std::vector<std::shared_ptr<std::mutex>> new_mutexes(new_table_size);
+      for (auto& m : new_mutexes) {
+        m = std::make_shared<std::mutex>();
+      }
+      mutexes_ = std::move(new_mutexes);
 
       for (auto& bucket : old_table) {
         for (auto& elem : bucket) {
@@ -179,7 +186,7 @@ class HashSetRefinable : public HashSetBase<T> {
 
       table_size_.store(new_table_size);
 
-      owner_.Set(nullptr, false);
+      is_resizing_ = false;
     }
   }
 };
